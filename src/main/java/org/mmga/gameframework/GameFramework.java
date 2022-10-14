99894d74e5d1b9870ad905628271c022559673f4
Wzp-2008 <minecraftwzpmc@163.com>
2022-10-14 23:48:06 +0800

    Initial Commit

---
+++ src/main/java/org/mmga/gameframework/GameFramework.java
@@ -0,0 +1,21 @@
+package org.mmga.gameframework;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author wzp
+ * @version 1.0.0
+ */
+public final class GameFramework extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
